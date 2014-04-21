25d82ef3691e1c1e6931ee2c0a6a1d09dcdd6944
MonicaB <mlbdenver@gmail.com>
2014-04-20 18:32:09 -0600

    Removed console logs

--- js/app.js
+++ js/app.js
@@ -3,22 +3,22 @@ $(document).ready(function () {
         if(counter%3===0) {
             if(counter%5===0) {
                 $("#container").append("<p>FizzBuzz</p>");
-                console.log("fizzbuzz");
+                //console.log("fizzbuzz");//
             }
             else {
                $("#container").append("<p>Fizz</p>");
-               console.log("fizz");
+               //console.log("fizz");//
             }
         }
         else {
             if(counter%5===0) {
             $("#container").append("<p>Buzz</p>");
-            console.log("buzz");
+            //console.log("buzz");//
             }
             else {
                 var result = "<p>" + counter + "</p>";
                 $("#container").append(result);
-                console.log(counter);
+                //console.log(counter);//
             }
         }
     }
